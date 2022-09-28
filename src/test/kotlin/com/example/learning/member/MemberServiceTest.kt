355b57359caea35161daa1c9bcf225e0ac820c69
owl3670 <gnltjr123@gmail.com>
2022-09-28 19:10:21 +0900

    회원 도메인 - 회원 가입 테스트

---
+++ src/test/kotlin/com/example/learning/member/MemberServiceTest.kt
@@ -0,0 +1,21 @@
+package com.example.learning.member
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MemberServiceTest {
+    val memberService: MemberService = MemberServiceImpl()
+
+    @Test
+    fun join() {
+        // given
+        val member = Member(1L, "memberA", Grade.VIP)
+
+        // when
+        memberService.join(member)
+        val findMember = memberService.findMember(1L)
+
+        // then
+        Assertions.assertThat(member).isEqualTo(findMember)
+    }
+}
