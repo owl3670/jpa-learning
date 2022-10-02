f5f9a70888782632e83722c08f6a4f2de0246bea
owl3670 <gnltjr123@gmail.com>
2022-10-02 14:25:58 +0900

    BeanDefinition

---
+++ src/test/kotlin/com/example/learning/beandefinition/BeanDefinitionTest.kt
@@ -0,0 +1,23 @@
+package com.example.learning.beandefinition
+
+import com.example.learning.AppConfig
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
+class BeanDefinitionTest {
+    val ac = AnnotationConfigApplicationContext(AppConfig::class.java)
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    fun findApplicationBean() {
+        val beanDefinitionNames = ac.beanDefinitionNames
+        for (beanDefinitionName in beanDefinitionNames) {
+            val beanDefinition = ac.getBeanDefinition(beanDefinitionName)
+            if (beanDefinition.role == BeanDefinition.ROLE_APPLICATION) {
+                println("beanDefinitionName = $beanDefinitionName beanDefinition = $beanDefinition")
+            }
+        }
+    }
+}
