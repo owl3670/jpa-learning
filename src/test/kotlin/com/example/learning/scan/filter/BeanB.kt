59746f3216cfb85c4eea47144e39d26855e9b78c
owl3670 <gnltjr123@gmail.com>
2022-10-11 15:40:21 +0900

    - 탐색 위치와 기본 스캔 대상

---
+++ src/test/kotlin/com/example/learning/scan/filter/BeanB.kt
@@ -0,0 +1,5 @@
+package com.example.learning.scan.filter
+
+@MyExcludeComponent
+class BeanB {
+}
---
+++ src/test/kotlin/com/example/learning/scan/filter/MyExcludeComponent.kt
@@ -0,0 +1,6 @@
+package com.example.learning.scan.filter
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class MyExcludeComponent()
