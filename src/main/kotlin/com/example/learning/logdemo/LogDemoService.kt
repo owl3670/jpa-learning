2587f2550c6cb8e26d4cf2ebc9f762556b0498a0
owl3670 <gnltjr123@gmail.com>
2022-10-21 10:45:09 +0900

    + request 스코프 예제 : 스코프와 provider

--- src/main/kotlin/com/example/learning/logdemo/LogDemoService.kt
+++ src/main/kotlin/com/example/learning/logdemo/LogDemoService.kt
@@ -1,13 +1,15 @@
 package com.example.learning.logdemo
 
 import com.example.learning.common.MyLogger
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Service
 
 @Service
 class LogDemoService(
-    private val myLogger: MyLogger
+    private val myLoggerProvider: ObjectProvider<MyLogger>
 ) {
     fun logic(id: String) {
+        val myLogger = myLoggerProvider.getObject()
         myLogger.writeLog("service id = $id")
     }
 }
--- src/main/kotlin/com/example/learning/web/LogDemoController.kt
+++ src/main/kotlin/com/example/learning/web/LogDemoController.kt
@@ -2,6 +2,7 @@ package com.example.learning.web
 
 import com.example.learning.common.MyLogger
 import com.example.learning.logdemo.LogDemoService
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -10,12 +11,13 @@ import javax.servlet.http.HttpServletRequest
 @Controller
 class LogDemoController (
     private val logDemoService: LogDemoService,
-    private val myLogger: MyLogger
+    private val myLoggerProvider: ObjectProvider<MyLogger>
         ){
     @RequestMapping("log-demo")
     @ResponseBody
     fun logDemo(req: HttpServletRequest): String {
         val requestURL = req.requestURL.toString()
+        val myLogger = myLoggerProvider.getObject()
         myLogger.setRequestUrl(requestURL)
         myLogger.writeLog("controller test")
         logDemoService.logic("testId")
