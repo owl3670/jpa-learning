18abb99fbeaaf5b642207d4638b8a8cbcdeff81e
owl3670 <gnltjr123@gmail.com>
2022-11-07 20:27:54 +0900

    + TemplateMethod 패턴 - 예제3

--- spring-advanced/src/test/kotlin/com/spring/advanced/trace/template/TemplateMethodTest.kt
+++ spring-advanced/src/test/kotlin/com/spring/advanced/trace/template/TemplateMethodTest.kt
@@ -1,5 +1,6 @@
 package com.spring.advanced.trace.template
 
+import com.spring.advanced.trace.template.code.AbstractTemplate
 import com.spring.advanced.trace.template.code.SubClassLogic1
 import com.spring.advanced.trace.template.code.SubClassLogic2
 import mu.KotlinLogging
@@ -36,5 +37,24 @@ class TemplateMethodTest {
         logic1.execute()
         val logic2 = SubClassLogic2()
         logic2.execute()
+    }
+
+    @Test
+    fun templateMethodV2() {
+        val template1 = object : AbstractTemplate() {
+            override fun call() {
+                logger.info("logic1 start")
+            }
+        }
+        logger.info("class name=${template1.javaClass.name}")
+        template1.execute()
+
+        val template2 = object : AbstractTemplate() {
+            override fun call() {
+                logger.info("logic2 start")
+            }
+        }
+        logger.info("class name=${template2.javaClass.name}")
+        template2.execute()
     }
 }
--- spring-advanced/src/test/kotlin/com/spring/advanced/trace/template/code/AbstractTemplate.kt
+++ spring-advanced/src/test/kotlin/com/spring/advanced/trace/template/code/AbstractTemplate.kt
@@ -1,12 +1,16 @@
 package com.spring.advanced.trace.template.code
 
+import mu.KotlinLogging
+
 abstract class AbstractTemplate {
+    private val logger = KotlinLogging.logger {}
+
     fun execute(){
         val startTime = System.currentTimeMillis()
         call()
         val endTime = System.currentTimeMillis()
         val elapsedTime = endTime - startTime
-        println("elapsedTime=$elapsedTime")
+        logger.info("elapsedTime=$elapsedTime")
     }
 
     protected abstract fun call()
