869a2f15dca7d3fa57c350b5bc43832156eb8cb9
owl3670 <gnltjr123@gmail.com>
2022-11-14 14:47:25 +0900

    + proxy pattern 과 decorator pattern - 프로젝트 생성

---
+++ spring-advanced-proxy/src/main/kotlin/com/spring/advanced/proxy/SpringAdvancedProxyApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.advanced.proxy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAdvancedProxyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAdvancedProxyApplication>(*args)
+}
