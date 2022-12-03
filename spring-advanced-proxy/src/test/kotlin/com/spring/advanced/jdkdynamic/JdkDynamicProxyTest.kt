33321bef6eadfe9f78748943ea13497a55824565
owl3670 <gnltjr123@gmail.com>
2022-12-03 15:03:08 +0900

    + JDK 동적 프록시 - 예제

---
+++ spring-advanced-proxy/src/test/kotlin/com/spring/advanced/jdkdynamic/JdkDynamicProxyTest.kt
@@ -0,0 +1,29 @@
+package com.spring.advanced.jdkdynamic
+
+import com.spring.advanced.jdkdynamic.code.*
+import org.junit.jupiter.api.Test
+import java.lang.reflect.Proxy
+
+class JdkDynamicProxyTest {
+    private val logger = mu.KotlinLogging.logger {}
+
+    @Test
+    fun dynamicA(){
+        val target : AInterface = AImpl()
+        val handler = TimeInvocationHandler(target)
+        val proxy : AInterface = Proxy.newProxyInstance(AInterface::class.java.classLoader, arrayOf(AInterface::class.java), handler) as AInterface
+        proxy.call()
+        logger.info("targetClass=${target.javaClass}")
+        logger.info("proxyClass=${proxy.javaClass}")
+    }
+
+    @Test
+    fun dynamicB(){
+        val target : BInterface = BImpl()
+        val handler = TimeInvocationHandler(target)
+        val proxy : BInterface = Proxy.newProxyInstance(BInterface::class.java.classLoader, arrayOf(BInterface::class.java), handler) as BInterface
+        proxy.call()
+        logger.info("targetClass=${target.javaClass}")
+        logger.info("proxyClass=${proxy.javaClass}")
+    }
+}
---
+++ spring-advanced-proxy/src/test/kotlin/com/spring/advanced/jdkdynamic/code/TimeInvocationHandler.kt
@@ -0,0 +1,20 @@
+package com.spring.advanced.jdkdynamic.code
+
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+
+class TimeInvocationHandler(
+    private val target: Any
+) : InvocationHandler {
+    private val logger = mu.KotlinLogging.logger {}
+
+    override fun invoke(proxy: Any, method: Method, args: Array<Any>?): Any {
+        logger.info("TimeProxy 실행")
+        val start = System.currentTimeMillis()
+        val result = method.invoke(target, *args ?: arrayOf())
+        val end = System.currentTimeMillis()
+        val resultTime = end - start
+        logger.info("TimeProxy 종료 resultTime=$resultTime")
+        return result
+    }
+}
