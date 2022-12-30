fc200c5aaf00db23cc8db65fc3adee2404eab184
owl3670 <gnltjr123@gmail.com>
2022-12-30 10:12:03 +0900

    + 스프링 AOP 구현 - 프로젝트 생성

---
+++ spring-advanced-aop/src/main/kotlin/com/example/spring/advanced/aop/SpringAdvancedAopApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring.advanced.aop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAdvancedAopApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAdvancedAopApplication>(*args)
+}
